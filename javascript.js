b6ffd4bdaf434316fdd06401358703525990896f
Erodri6z <erikrodriguez2417@gmail.com>
2023-08-03 23:04:47 -0500

    use typescript to compile ts to js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 let myJSFood = "popcorn"
 
 
+
+myJSFood.sort()
+
 // myJSFood.push('beef')
 
 //notice how there is no linting, there is in ts
--- typescript.js
+++ typescript.js
@@ -1,44 +1,26 @@
 // let food: string = 'Popcorn'
-
 // defining it as a string as seen above is something that is only really needed if you will try to declare it again.
 // Its always best to over define the types and not let it infer.
-
 //one of the big places typescript shines is in catching errors.
-
 // let myMeal = true
-
 // myMeal.push('beef')
-
 // ^^ notice the linting
-
 // remember that TypeScript only matters when in development. Once we are done it will be transcribed/compiled into regular JavaScript
-
 // Keep in mind, you cannot write typescript in node
-
-let myTSFood: string = 'twizzlers'
-
+var myTSFood = 'twizzlers';
 //static typing 
-
-let thisIsAnything
-
-thisIsAnything = 1
-
-thisIsAnything = 'david'
-
-thisIsAnything = false
-
+var thisIsAnything;
+thisIsAnything = 1;
+thisIsAnything = 'david';
+thisIsAnything = false;
 // any is a great source of errors, so lets try to avoid this 
-
-let turn, winner, tie, board
-
+var turn, winner, tie, board;
 // other way to do board is board: Array<number> but its not the best way for this situation
-
 function init() {
-  turn = 1
-  winner = false 
-  tie = false
-  board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  console.log(turn)
+    turn = 1;
+    winner = false;
+    tie = false;
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    console.log(turn);
 }
-
-init()
+init();
